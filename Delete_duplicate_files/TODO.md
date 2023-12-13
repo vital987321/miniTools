8742a2f0b0564cff4068cff8c6aade2ce4669de7
Vitalii Velychko <velychko@axl.local>
2023-12-13 17:03:16 +0100

    working on PermissionError

--- Delete_duplicate_files/TODO.md
+++ Delete_duplicate_files/TODO.md
@@ -2,5 +2,6 @@ In version 0:
 1. Handle 'read only file'  exception.
 2. Check files after removal. Print how may were really removed.
 3. What happens if file is open?
+4. If file is already deleted/moved?
 4. consider splitting code into separate functions.
 5. Write description.
--- Delete_duplicate_files/delete_duplicate_files.py
+++ Delete_duplicate_files/delete_duplicate_files.py
@@ -1,8 +1,13 @@
 __version__='0'
 
 import os
-folder1=input('Enter full address of folder 1: ')
-folder2=input('Enter full address of folder 2: ')
+while True:
+    folder1=input('Enter full address of folder 1: ')
+    folder2=input('Enter full address of folder 2: ')
+    if folder1==folder2:
+        print('folder 1 and folder 2 must be different.')
+    else:
+        break
 
 files1 = [f for f in os.listdir(folder1) if os.path.isfile(os.path.join(folder1,f))]
 files2 = [f for f in os.listdir(folder2) if os.path.isfile(os.path.join(folder2,f))]
@@ -17,10 +22,16 @@ if len(duplicates)>0:
         print(file)
     isdelete=input(f'\n!!! Do you want to delete these files from {folder1} ? (y/n): ')
     if isdelete.lower()=='y':
+        nopermission_files=[]
+        deleted=0
         for file in duplicates:
-            os.remove(os.path.join(folder1,file))
-        print(f'{len(duplicates)} file(s) deleted.')
+            try:
+                os.remove(os.path.join(folder1,file))
+                deleted+=1
+            except PermissionError:
+                nopermission_files.append(file)
+        print(f'{deleted} file(s) deleted.')
     else:
         print('Files are not deleted.')
 else:
-    print('No duplicates found.')
+    print('Nothing is deleted.')
